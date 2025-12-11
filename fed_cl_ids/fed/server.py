a6b6bba89c8854c4b3c5906c373b45707709e2e6
Frank Doan <Tullskon@gmail.com>
2025-12-11 18:51:52 -0500

    Server.py uses pathlib now

--- fed_cl_ids/fed/server.py
+++ fed_cl_ids/fed/server.py
@@ -1,10 +1,11 @@
 """Starts federated learning from simulation and configuration file"""
 from typing import Optional, Iterable
 from collections import OrderedDict
+from dataclasses import dataclass
+from pathlib import Path
 import hashlib
 import json
 import time
-import os
 
 from torch import Tensor
 import torch
@@ -12,13 +13,21 @@ import yaml
 import pandas as pd
 
 from flwr.app import ArrayRecord, ConfigRecord, Context
+from flwr.serverapp.strategy import Result
 from flwr.serverapp import Grid, ServerApp
 
 from sklearn.model_selection import train_test_split
 from fed_cl_ids.fed.custom_strategies import UAVIDSFedAvg
 from fed_cl_ids.models.mlp import MLP
 
-class Configuration:
+MAIN_PATH = Path().cwd() / 'fed_cl_ids'
+UAVIDS_DAYS_PATH = MAIN_PATH / 'data_pipeline' / 'splits' / 'uavids_days.yaml'
+UAVIDS_DATA_PATH = MAIN_PATH / 'datasets' / 'UAVIDS-2025 Preprocessed.csv'
+RUNTIME_PATH = MAIN_PATH / 'runtime'
+OUTPUT_PATH = MAIN_PATH / 'outputs'
+
+@dataclass()
+class ServerConfiguration:
     """Stores configurations from pyproject.toml"""
     def __init__(self, grid: Grid, context: Context) -> None:
         self.fraction_train = float(context.run_config['fraction-train'])
@@ -33,7 +42,7 @@ class Configuration:
 class Server:
     """Main class holding server configurations and strategy."""
     def __init__(self, grid: Grid, context: Context) -> None:
-        self.config = Configuration(grid, context)
+        self.config = ServerConfiguration(grid, context)
         self.federated_model = UAVIDSFedAvg(
             fraction_train=self.config.fraction_train,
             fraction_eval=self.config.fraction_evaluate,
@@ -41,7 +50,7 @@ class Server:
         self.current_parameters = self._initial_parameters(context)
 
         self.dataframe: pd.DataFrame
-        self.dataframe_path: str = ''
+        self.dataframe_path: Optional[Path] = None
 
     def _initial_parameters(self, context: Context) -> OrderedDict[str, Tensor]:
         widths = str(context.run_config['mlp-widths'])
@@ -57,10 +66,10 @@ class Server:
 
     def split_data(
             self,
-            raw_flows: list[int], # Iterable
+            raw_flows: list[int],
             train_ratio: float = 0.8,
             random_seed: Optional[int] = None,
-            csv_path: Optional[str] = None) -> list[list[int]]:
+            csv_path: Optional[Path] = None) -> list[list[int]]:
         """
         Splits a list of flow IDs into two datasets.
         Can optionally be stratified by passing in csv filepath.
@@ -73,13 +82,13 @@ class Server:
         :type random_seed: Optional[int]
         :param csv_path: Location of csv file to allow label stratification. 
         Stratifies by the 'label' column.
-        :type csv_path: Optional[str]
+        :type csv_path: Optional[Path]
         :return: Two lists (from train_test_split) of integers.
         :rtype: list[list[int]]
         """
         labels: Optional[pd.Series] = None
         if csv_path:
-            if not self.dataframe_path or self.dataframe_path != csv_path:
+            if self.dataframe_path is None or self.dataframe_path != csv_path:
                 self.dataframe_path = csv_path
                 self.dataframe = pd.read_csv(
                     filepath_or_buffer=csv_path,
@@ -115,17 +124,6 @@ class Server:
             clients[i].append(flow_id)
         return clients
 
-def clear_directory(path: str) -> None:
-    """
-    Removes all files in a folder directory
-
-    :param path: Path to the folder
-    :type path: str
-    """
-    for file_name in os.listdir(path):
-        file_path = os.path.join(path, file_name)
-        os.remove(file_path)
-
 app = ServerApp()
 
 @app.main()
@@ -138,24 +136,21 @@ def main(grid: Grid, context: Context) -> None:
     :param context: Description
     :type context: Context
     """
+
+    try:
+        RUNTIME_PATH.mkdir()
+    except FileExistsError:
+        clear_directory(RUNTIME_PATH)
+
     server = Server(grid, context)
-
-    uavids_path = "fed_cl_ids/data_pipeline/splits/uavids_days.yaml"
-    with open(uavids_path, encoding='utf-8') as file:
-        raw_days: dict[str, list[int]]= yaml.safe_load(file)
-    # Assuming dict is sorted/ordered by days
-    filtered_days = tuple(raw_days.items())[:server.config.n_days]
-    uavids_days: dict[str, list[int]] = dict(filtered_days)
-
-    runtime_path = os.path.join("fed_cl_ids", "runtime")
-    os.makedirs(runtime_path, exist_ok=True)
-    clear_directory(runtime_path)
+    uavids_days = get_uavids(server, UAVIDS_DAYS_PATH)
 
     start = time.time()
     for day, raw_flows in enumerate(uavids_days.values(), 1):
-        data_path = "fed_cl_ids/datasets/UAVIDS-2025 Preprocessed.csv"
-        splits = server.split_data(raw_flows, csv_path=data_path)
-        train_flows, evaluate_flows = splits
+        train_flows, evaluate_flows = server.split_data(
+            raw_flows=raw_flows,
+            csv_path=UAVIDS_DATA_PATH
+        )
         train_flows = Server.distribute_flows(
             flows=train_flows,
             n_clients=server.config.n_train_clients
@@ -177,22 +172,52 @@ def main(grid: Grid, context: Context) -> None:
             train_config=train_config,
             evaluate_config=evaluate_config,
         )
-
-        # I assume the resulting arrays are already in cpu. Remove?
-        server.current_parameters = OrderedDict({
-            key: state.cpu()
-            for key, state in result.arrays.to_torch_state_dict().items()
-        })
-        torch.save(server.current_parameters, f"fed_cl_ids/outputs/Day{day}.pt")
-        metrics = result.evaluate_metrics_clientapp.popitem()
-        with open("fed_cl_ids/outputs/metrics.txt", 'a', encoding='utf-8') as file:
-            file.write(
-                f"Day {day}: "
-                f"{server.config.n_train_clients}/{server.config.total_clients} "
-                f"clients: {str(metrics)}\n"
-            )
-
-    with open("fed_cl_ids/outputs/metrics.txt", 'a', encoding='utf-8') as file:
+        log_results(server, result, day)
+    with open(OUTPUT_PATH / 'metrics.txt', 'a', encoding='utf-8') as file:
         file.write('\n')
-    clear_directory(runtime_path)
+    clear_directory(RUNTIME_PATH)
     print(time.time() - start)
+
+def clear_directory(path: Path) -> None:
+    """
+    Removes all files in a folder directory
+
+    :param path: Path to the folder
+    :type path: Path
+    """
+    for item in path.iterdir():
+        if item.is_file():
+            item.unlink()
+
+def get_uavids(server: Server, filepath: Path) -> dict[str, list[int]]:
+    """
+    Opens form yaml file, and filters days from configuration file
+    
+    :param server:
+    :type server: Server
+    :param filepath: Path to yaml file containing flows IDs
+    :type filepath: Path
+    :return: Contains day as a str and all the flow IDs
+    :rtype: dict[str, list[int]]
+    """
+    with open(filepath, encoding='utf-8') as file:
+        raw_days: dict[str, list[int]] = yaml.safe_load(file)
+    # Assuming dict is sorted/ordered by days
+    filtered_days = tuple(raw_days.items())[:server.config.n_days]
+    return dict(filtered_days)
+
+def log_results(server: Server, result: Result, day: int) -> None:
+    """Saves aggregated model as pt file and logs aggregated metrics"""
+    # I assume the resulting arrays are already in cpu. Remove?
+    server.current_parameters = OrderedDict({
+        key: state.cpu()
+        for key, state in result.arrays.to_torch_state_dict().items()
+    })
+    torch.save(server.current_parameters, OUTPUT_PATH / f'Day{day}.pt')
+    metrics = result.evaluate_metrics_clientapp.popitem()
+    with open(OUTPUT_PATH / 'metrics.txt', 'a', encoding='utf-8') as file:
+        file.write(
+            f"Day {day}: "
+            f"{server.config.n_train_clients}/{server.config.total_clients} "
+            f"clients: {str(metrics)}\n"
+        )
